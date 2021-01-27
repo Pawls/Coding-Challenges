40545a89f0e6f72f226f789a35b63567699a5344
Paul Davis <43160081+Pawls@users.noreply.github.com>
2021-01-27 13:04:22 -0600

    Update JavaAnagrams.java

--- Java/JavaAnagrams.java
+++ Java/JavaAnagrams.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 public class Solution {
 
     static boolean isAnagram(String a, String b) {
-        a = a.toLowerCase();
-        b = b.toLowerCase();
-        int checker = 0;
-        for (char c : a.toCharArray()) {
-          checker ^= (int) c;
+        if (a.length() != b.length()) return false;
+        int[] primes = {3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103};
+        int hash_a = 1;
+        int hash_b = 1;
+        for (int i = 0; i < a.length(); i++) {
+            int c1 = (((int) a.charAt(i)) & ~32) - 65;
+            int c2 = (((int) b.charAt(i)) & ~32) - 65;
+            hash_a *= primes[c1];
+            hash_b *= primes[c2];
         }
-        for (char c : b.toCharArray()) {
-          checker ^= (int) c;
-        }
-        return checker == 0;
+        return hash_a == hash_b;
     }
 
   public static void main(String[] args) {
